cf42f87d492a4c59e157c871f97652cbf088d0ab
Max Duijsens <maxduijsens@gmail.com>
2014-11-18 15:26:51 +0100

    Initial test

---
+++ Db.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+
+public class Db {
+	
+	private Connection c;
+	
+	public Db() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+	}
+	
+	
+	
+}
