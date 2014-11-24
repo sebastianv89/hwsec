99464c355b31be75e59fd30b22a7b3a262bac1ca
sasha121 <aniketchaudhari_23@hotmail.com>
2014-11-24 23:45:23 +0100

    personal terminal
    smart  card should be created... but dont know where it should be create
    ....in backend or database

--- src/backend/PersonalTerminal.java
+++ src/backend/PersonalTerminal.java
@@ -1,8 +1,39 @@
 package backend;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.math.BigInteger;
+import java.util.List;
+
+import javax.swing.*;
+
+import java.security.*;
+import java.security.spec.*;
+import java.security.interfaces.*;
+
+import javax.smartcardio.*;
+
 public class PersonalTerminal {
 	// the personal terminal should upload a fresh secret key and certificate to a smartcard
 	// the environment is secure by assumption, so no auth is required
+	//Get certificate from the backend and send it to the card by addCertificate
 	
 	// use: Backend.registerNewCard(customerId)
+	// call this function "InitData registerNewCard"
+	// create smartcard object 
+	
+	public static void main(String[] args){
+		//InitData nc = new InitData(cert, keypair.getPrivate(), certVerifKey);
+		Backend bk = new Backend();
+		InitData nc = bk.registerNewCard(9, 1);
+		
+		byte[] pk = nc.privateKey;
+		byte[] c = nc.certificate;
+		byte[] ca = nc.caVerifKey;
+		
+		
+		
+		
+	}
 }
