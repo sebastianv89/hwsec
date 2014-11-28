f7cc8be2780c65b4baeb77dc0f293abd4f5807d3
fitriandini <fitria.andini@gmail.com>
2014-11-28 12:58:32 +0100

    update function for the register new customer

--- Backend/src/backend/BackendRentalTerminal.java
+++ Backend/src/backend/BackendRentalTerminal.java
@@ -8,9 +8,21 @@ import terminal.Card;
 public class BackendRentalTerminal {
 	Database db = new Database();
 	Backend be = new Backend();
+	Serialization serial = new Serialization();
+	ByteUtils byteUtil = new ByteUtils();
+	
+	int EXP_LENGTH = 8;
+	int PUBKEY_LENGTH = 162;
 	
 	public BackendRentalTerminal(){
 		
+	}
+	
+	public byte[] ReadCertFromSmartCard(){
+		byte[] cert = null;
+		
+		
+		return cert;
 	}
 	
 	public boolean MutualAuthenticationRT_S(){
@@ -22,12 +34,44 @@ public class BackendRentalTerminal {
 		
 	}
 	
+	/* TODO: Change exp to short
+	 * Certificate with expdate is a byte array that looks like:
+	 * cert[0] = type (0 = smartcard, 1 = rentalterm, 2 = vehicleterm)
+	 * cert[1..163] = rsapublickey (length 162bytes)
+	 * cert[164..172] = expiration date of type long (8bytes)
+	 * cert[173...301] = Signature (128bytes)
+	 */ 
+	
 	public void RegisterNewCustomer(String name){
-		//add to database customer
-		Integer customerId = db.insertCustomer(name);
+		/**
+		 * REGISTER NEW CUSTOMER
+		 * 1. Read cert from card
+		 * 2. renew the certificate to expand the expiration date
+		 * 3. Add customer and card data to database. NOTE: pubkey database is string
+		 */
 		
-		//add new smartcard
-		InitData init = be.registerNewCard(customerId);
+		//1. read from the card 
+		byte[] cert = ReadCertFromSmartCard();
+		//2a. renew the certificate
+		byte[] newCert = null;
+		try {
+			newCert = be.renewCertificate(cert);
+		} catch (RevokedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//2b. extract the expiration date and pubkey from card certificate
+		byte[] exp = new byte[EXP_LENGTH];
+		byte[] pubKey = new byte[PUBKEY_LENGTH];
+		System.arraycopy(newCert, 164, exp, 0, EXP_LENGTH);
+		System.arraycopy(newCert, 1, pubKey, 0, PUBKEY_LENGTH);
+		String strPubKey = serial.SerializeByteKey(pubKey);
+		long expLong = byteUtil.bytesToLong(exp);
+		
+		//3. add to database customer
+		Integer customerId = db.insertCustomer(name);
+		db.addSmartcard(customerId, expLong, strPubKey);
+		
 	}
 	
 	/**
@@ -61,12 +105,13 @@ public class BackendRentalTerminal {
 	
 	public Card getCardDB(byte[] cardPublicKey){
 		Card card = new Card();
+		String strPubKey = serial.SerializeByteKey(cardPublicKey);
 		
-		ResultSet rs = db.selectCard(cardPublicKey);
+		ResultSet rs = db.selectCard(strPubKey);
 		try {
 			if(rs.next()){
 				card = new Card(rs.getInt("id"), rs.getInt("custID"), rs.getString("custName"), 
-						rs.getInt("km"), rs.getLong("exp") , rs.getInt("revoke"), rs.getBytes("cardPK"));
+						rs.getInt("km"), rs.getLong("exp") , rs.getInt("revoke"), rs.getString("cardPK"));
 			}else{
 				System.out.println("That card has not been issued");
 				card = null;
