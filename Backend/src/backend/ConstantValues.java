df0b84c9c02edfdf6e53dce5c2975cd41a8a3bf0
Max Duijsens <maxduijsens@gmail.com>
2014-11-29 16:31:51 +0100

    Add pubexponent bigint

--- Backend/src/backend/ConstantValues.java
+++ Backend/src/backend/ConstantValues.java
@@ -1,11 +1,14 @@
 package backend;
 
+import java.math.BigInteger;
+
 public class ConstantValues {
 
 	public int RSAPUBLICKEYLENGTH = 162;
 	public int PUBMODULUS = 128; // after you strip off the first byte (which = 0)
 	public int PUBEXPONENT = 3;
 	public int PRIVMODULUS = 128; // after you strip off the first byte (which = 0)
+	public BigInteger PUBEXPONENT_BYTE = new BigInteger("65537".getBytes());
 	public int PRIVEXPONENT = 128; // after you strip off the first byte (which = 0)
 	public int EXP_LENGTH = 8;
 	public int PK_EXP_LENGTH = 170; //162+8
@@ -15,6 +18,7 @@ public class ConstantValues {
 	
 	public int CARDCERT_LENGTH = 265;
 	public int TERMINALCERT_LENGTH = 257;
+	
 
 
 }
