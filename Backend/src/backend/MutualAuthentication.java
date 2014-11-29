5c814bee1d812222b8faa2374427b22c9ec4dca0
fitriandini <fitria.andini@gmail.com>
2014-11-29 19:56:51 +0100

    finished the mutual authentication (without card communication and session key)

--- Backend/src/backend/MutualAuthentication.java
+++ Backend/src/backend/MutualAuthentication.java
@@ -12,9 +12,7 @@ import java.security.SignatureException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.RSAPublicKeySpec;
-import java.security.spec.X509EncodedKeySpec;
 
 import javax.crypto.Cipher;
 
@@ -23,37 +21,39 @@ public class MutualAuthentication {
 	ConstantValues CV = new ConstantValues();
 	Serialization serial = new Serialization();
 	
-	int NOUNCE_LENGTH = 16;
+	int NONCE_LENGTH = 16;
 	
 	public MutualAuthentication(){
 		
 	}
 	
 	/**
-	 * Rental Terminal Authentication:
+	 * Terminal Mutual Authentication:
 	 * 1. Read RT Certificate from file (OK)
 	 * 2. Generate Nonce (OK)
 	 * 3. Combine those two and split them into two package; (OK)
-	 * 4. Send those two package to card (OK)
-	 * 5. Received 4 packages from smartcard (ask the card for the every next package; mentioned the package number in P2)
+	 * 4. Send those two package to card --
+	 * 5. Received 4 packages from smartcard --
+	 * (ask the card for the every next package; mentioned the package number in P2) --
 	 * 6. Read RT Priv Key from file (OK)
 	 * 7. Decrypt every packages (OK)
 	 * 8. Combine the decrypted packages (OK)
 	 * 9. Split the smartcard's certificate (certS) and Data (N, Ktmp) from the decrypted packages (OK)
-	 * 10.Verify the Certificate (with the CAPublicKey - from file - and check the revocation status)
-	 * 11. Verify the card Signature (data, cardPubKey, dataSignature)
-	 * 12. Send the session key to card (later)
+	 * 10.Verify the Certificate (with the CAPublicKey - from file - and check the revocation status) (OK)
+	 * 11. Verify the card Signature (data, cardPubKey, dataSignature) (Max help. OK)
+	 * 12. Send the session key to card (later) --
 	 */
 	
-	public void TerminalMutualAuth(byte[] cert, RSAPrivateKey privKey){
+	public boolean TerminalMutualAuth(byte[] cert, RSAPrivateKey privKey){
+		boolean states = false;
 		
 		//byte[] rtCert = readFiles(certFilename);  //Read Certificate for  Terminal
-		byte[] nounce = util.GenerateRandomBytes(NOUNCE_LENGTH); //generate nonces
+		byte[] nounce = util.GenerateRandomBytes(NONCE_LENGTH); //generate nonces
 		
 		//Split the certificate into two package
 		byte[] pack1 = new byte[CV.PUBMODULUS + 1];  //129
 		System.arraycopy(cert, 0, pack1, 0, pack1.length); 
-		byte[] pack2 = new byte[CV.SIG_LENGTH + NOUNCE_LENGTH]; //SIGNATURE + NOUNCE = 144
+		byte[] pack2 = new byte[CV.SIG_LENGTH + NONCE_LENGTH]; //SIGNATURE + NONCE = 144
 		System.arraycopy(cert, CV.PUBMODULUS+1, pack2, 0, CV.SIG_LENGTH);
 		System.arraycopy(nounce, 0, pack2, CV.SIG_LENGTH, nounce.length);
 		
@@ -75,32 +75,43 @@ public class MutualAuthentication {
 		//combine the decrypted package
 		byte[] scPack = serial.combineThePackage(scDataPack1, scDataPack2, scDataPack3, scDataPack4);
 		
-		//split card certificate and the card data {N, Ktmp}
+		//split card certificate and the card data {N, Ktmp}Sig
 		byte[] cardCert = new byte[CV.CARDCERT_LENGTH];
-		System.arraycopy(scPack, 0, cardCert, 0, CV.CARDCERT_LENGTH);
+		System.arraycopy(scPack, 0, cardCert, 0, CV.CARDCERT_LENGTH);		
 		byte[] cardData = new byte[CV.CARDDATA_LENGTH];
 		System.arraycopy(scPack, CV.CARDCERT_LENGTH, cardData, 0, CV.CARDDATA_LENGTH);
-		
-		
-		/*//for testing purposes
-		new Random().nextBytes(scPack1);
-		new Random().nextBytes(scPack2);
-		RSAPublicKey rtPubKey = GetPublicKeyFromFile("RTPublicKey");
-		System.out.println(Arrays.toString(scPack));
-		byte[] enc = RSAEncrypt(scPack, rtPubKey);
-		System.out.println(Arrays.toString(enc));
-		System.out.println(enc.length);*/
-		
-		
-		
-		
-		
+		//get card public key (from cardCert)
+		byte[] certPubKey = serial.getPublicKeyFromCert(cardCert);
+		//get signature from cardData
+		byte[] cardDataSig = serial.getSigFromCert(cardData);
+		//Verify the certificate
+		if(certVerify(cardCert)){
+			//Verify the cardData {N, Ktmp} signature
+			if(sigVerif(cardData, certPubKey, cardDataSig)){
+				//TODO send session key to card
+				states = true;
+			}
+		}
+		return states;
 	}
 	
-	public boolean certVerify(byte[] cert){
+	/**
+	 * Init Verify with CApubKey
+	 * Update with card cert data 
+	 * sig.verify with card signature
+	 * @param certSC
+	 * @return
+	 */
+	public boolean certVerify(byte[] cardCert){
 		boolean result = false;
 		byte[] CApubKey = readFiles("CAPublicKey");
+		//split the certificate data and the signature 
+		byte[] certData = serial.getCardCertDataFromCert(cardCert); //without signature
+		byte[] certSig = serial.getSigFromCert(cardCert); //card signature
 		
+		if(sigVerif(certData, CApubKey, certSig)){
+			result = true;
+		}
 		
 		return result;
 	}
@@ -164,48 +175,13 @@ public class MutualAuthentication {
 		return bytes;
 	}
 	
-	private RSAPrivateKey GetPrivateKeyFromFile(String filename){
-		byte[] privateKeyBytes = readFiles(filename);
-		RSAPrivateKey privKey = null;
-		try {
-			privKey = (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(privateKeyBytes));
-		} catch (InvalidKeySpecException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return privKey;
-	}
-	
-	
-	private RSAPublicKey GetPublicKeyFromFile(String filename){
-		byte[] publicKeyBytes = readFiles(filename);
-		RSAPublicKey pubKey = null;
-		X509EncodedKeySpec pubspec = new X509EncodedKeySpec(publicKeyBytes);
-		KeyFactory factory;
-		
-		try {
-			factory = KeyFactory.getInstance("RSA");
-			pubKey = (RSAPublicKey) factory.generatePublic(pubspec);
-		}catch (NoSuchAlgorithmException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}catch (InvalidKeySpecException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return pubKey;
-	}
 	
 	
 	public byte[] RSADecrypt(byte[] ciphertext, RSAPrivateKey privatekey) {
 		byte[] plaintext = null;
 		try {
 			/* Create cipher for decryption. */
-			Cipher decrypt_cipher = Cipher.getInstance("ALG_RSA_ISO14888");
+			Cipher decrypt_cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 			decrypt_cipher.init(Cipher.DECRYPT_MODE, privatekey);
 
 			/* Reconstruct the plaintext message. */
@@ -219,20 +195,20 @@ public class MutualAuthentication {
 	}
 	
 	
-	
+	/*
 	public byte[] RSAEncrypt(byte[] plaintext, RSAPublicKey publicKey) {
 		 byte[] ciphertext = null;
 	      try {
-	         /* Create a cipher for encrypting. */
-	         Cipher encrypt_cipher = Cipher.getInstance("ALG_RSA_ISO14888" );
+	         // Create a cipher for encrypting.
+	         Cipher encrypt_cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 	         encrypt_cipher.init(Cipher.ENCRYPT_MODE, publicKey);
 
-	         /* Encrypt the secret message and store in file. */
+	         // Encrypt the secret message and store in file.
 	        ciphertext = encrypt_cipher.doFinal(plaintext);
 	      } catch (Exception e) {
 	         e.printStackTrace();
 	      }
 	      return ciphertext;
-	   }
+	   }*/
 
 }
