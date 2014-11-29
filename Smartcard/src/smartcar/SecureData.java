23073b205ea959ed097061a3a183ac34ef4141bf
Sebastian <sebastian.verschoor@gmail.com>
2014-11-29 18:52:29 +0100

    Implemented mutual auth in the smartcard

--- Smartcard/src/smartcar/SecureData.java
+++ Smartcard/src/smartcar/SecureData.java
@@ -1,12 +1,15 @@
 package smartcar;
 
-import javacard.security.Signature;
 import javacard.framework.JCSystem;
 import javacard.framework.Util;
 import javacard.security.KeyBuilder;
 import javacard.security.RSAPrivateKey;
 import javacard.security.RSAPublicKey;
+import javacard.security.RandomData;
+import javacard.security.Signature;
 import javacardx.crypto.Cipher;
+
+//TODO: transactions
 
 /**
  * Class for holding all persistent security data
@@ -14,32 +17,40 @@ import javacardx.crypto.Cipher;
 public class SecureData {
 
 	// sizes of data
-	public static final short SIZE_CARD_CERTIFICATE_DATA = 137;
-	public static final short SIZE_TERM_CERTIFICATE_DATA = 129;
-	public static final short SIZE_CERTIFICATE_SIG = 128;
-	public static final short SIZE_RSAKEY_EXP = 128;
-	public static final short SIZE_RSAKEY_MOD = 128;
+	public static final short SIZE_RSA_KEY_PRIV_EXP = 128;
+	public static final short SIZE_RSA_KEY_MOD = 128;
+	public static final short SIZE_RSA_KEY_PUB_EXP = 3;
+	public static final short SIZE_RSA_SIG = 128;
+	public static final short SIZE_NONCE = 16;
+	public static final short SIZE_CERT_TYPE = 1;
+	public static final short SIZE_CERT_EXP = 8;
+	public static final short SIZE_CERT_DATA_CARD = (short) ((short) (SIZE_CERT_TYPE + SIZE_RSA_KEY_MOD) + SIZE_CERT_EXP);
+	public static final short SIZE_CERT_DATA_TERM = (short) (SIZE_CERT_TYPE + SIZE_RSA_KEY_MOD);
+	public static final short SIZE_CERT_CARD = (short) (SIZE_CERT_DATA_CARD + SIZE_RSA_SIG);
+	public static final short SIZE_CERT_TERM = (short) (SIZE_CERT_TYPE + SIZE_RSA_SIG);
+	public static final short SIZE_PUBENC_PLAIN = (short) (SIZE_RSA_KEY_MOD - 11);
+	public static final short SIZE_PUBENC_CIPH = 128;
 
-	// crypt objects
+	private static final byte[] RSA_PUB_EXP = { 0x01, 0x00, 0x01 };
+
+	// permanent crypto objects
 	private RSAPrivateKey signatureKey; // TODO: replace completely with signer
-	RSAPublicKey caVerificationKey; // TODO: replace completely with caVerifier
-	RSAPublicKey[] termEncryptKey; // RAM
-	Signature signer;
-	Signature caVerifier;
-	Cipher termEncrypter;
+	private RSAPublicKey caVerificationKey; // TODO: replace completely with
+	// caVerifier
+	private byte[] certificate; // raw data
+	private RandomData rng;
 
-	// TODO: check if the public RSA exponent is always the same
-	private static final byte[] RSA_EXP = { 0x01, 0x00, 0x01 };
-	private static final short RSA_EXP_SIZE = 0x0003;
+	private Signature signer;
+	private Signature caVerifier;
+	private Cipher termEncrypter;
 
-	// certificate is stored raw, easiest for just sending data
-	private byte[] certificate;
+	// transient crypto objects
+	private RSAPublicKey[] termEncryptKey;
 
 	/** Constructor, allocates data, initializes crypto */
 	SecureData() {
 		// allocate data
-		certificate = new byte[SIZE_CARD_CERTIFICATE_DATA
-				+ SIZE_CERTIFICATE_SIG];
+		certificate = new byte[SIZE_CERT_CARD];
 		termEncryptKey = (RSAPublicKey[]) JCSystem.makeTransientObjectArray(
 				(short) 1, JCSystem.CLEAR_ON_DESELECT);
 
@@ -49,24 +60,42 @@ public class SecureData {
 		caVerificationKey = (RSAPublicKey) KeyBuilder.buildKey(
 				KeyBuilder.TYPE_RSA_PUBLIC, KeyBuilder.LENGTH_RSA_1024, false);
 		// set the constant public exponent for the CAKey
-		caVerificationKey.setExponent(RSA_EXP, (short) 0, RSA_EXP_SIZE);
+		caVerificationKey.setExponent(RSA_PUB_EXP, (short) 0,
+				SIZE_RSA_KEY_PUB_EXP);
 
 		// initialize "the workers"
 		caVerifier = Signature.getInstance(Signature.ALG_RSA_MD5_PKCS1, false);
 		signer = Signature.getInstance(Signature.ALG_RSA_MD5_PKCS1, false);
-		termEncrypter = Cipher.getInstance(Cipher.ALG_RSA_ISO14888, false);
-		Cipher.ALG_RSA
+		termEncrypter = Cipher.getInstance(Cipher.ALG_RSA_PKCS1, false);
+		rng = RandomData.getInstance(RandomData.ALG_SECURE_RANDOM);
+	}
+
+	void publicEncrypt(byte[] plaintext, byte counter, byte[] ciphertext) {
+		short inOffset = (short) (counter * SIZE_PUBENC_PLAIN);
+		short inSize = (counter == 4 ? 1024 : SIZE_PUBENC_PLAIN); // FIXME:
+																	// length
+		termEncrypter.doFinal(plaintext, inOffset, inSize, ciphertext,
+				(short) 0);
+	}
+
+	/** Create the response for mutual authentication in the tmp buffer */
+	void createAuthResponse(byte[] buffer) {
+		// place the certificate
+		Util.arrayCopy(certificate, (short) 0, buffer, (short) 0,
+				SIZE_CERT_CARD);
+
+		// generate temporary key
+		rng.generateData(buffer, (short) (SIZE_CERT_CARD + SIZE_NONCE),
+				SIZE_NONCE);
+		// sign nonce + tmpkey
+		signer.sign(buffer, SIZE_CERT_CARD, (short) (2 * SIZE_NONCE), buffer,
+				(short) (SIZE_CERT_CARD + (short) (2 * SIZE_NONCE)));
 	}
 
 	/** Validate a terminal certificate */
-	boolean validateCert(byte[] data, short dataOfs, byte[] sig, short sigOfs,
-			byte type) {
-		// validate certificate type
-		if (data[dataOfs++] != type) {
-			return false;
-		}
-		return caVerifier.verify(data, dataOfs, SIZE_TERM_CERTIFICATE_DATA,
-				sig, sigOfs, SIZE_CERTIFICATE_SIG);
+	boolean validateCert(byte[] data, short dataOfs, byte[] sig, short sigOfs) {
+		return caVerifier.verify(data, dataOfs, SIZE_CERT_DATA_TERM, sig,
+				sigOfs, SIZE_RSA_SIG);
 	}
 
 	/** Initialize the workers after personalization */
@@ -77,43 +106,38 @@ public class SecureData {
 
 	/** Set the (public) key modulus (N) for the encryption key */
 	void setTermEncryptKeyMod(byte[] buffer, short offset) {
-		termEncryptKey[0].setModulus(buffer, offset, SIZE_RSAKEY_MOD);
-		termEncryptKey[0].setExponent(RSA_EXP, (short) 0, RSA_EXP_SIZE);
+		termEncryptKey[0].setModulus(buffer, offset, SIZE_RSA_KEY_MOD);
+		termEncryptKey[0].setExponent(RSA_PUB_EXP, (short) 0,
+				SIZE_RSA_KEY_PUB_EXP);
 		termEncrypter.init(termEncryptKey[0], Cipher.MODE_ENCRYPT);
 	}
 
 	/** Set the (private) key exponent (d) for the signature key */
 	void setSignKeyExp(byte[] buffer, short offset) {
-		signatureKey.setExponent(buffer, offset, SIZE_RSAKEY_EXP);
+		signatureKey.setExponent(buffer, offset, SIZE_RSA_KEY_PRIV_EXP);
 	}
 
 	/** Set the (public) key modulus (N) for the signature key */
 	void setSignKeyMod(byte[] buffer, short offset) {
-		signatureKey.setModulus(buffer, offset, SIZE_RSAKEY_MOD);
+		signatureKey.setModulus(buffer, offset, SIZE_RSA_KEY_MOD);
 	}
 
 	/** Set the (public) key modulus (N) for the CA verification key */
 	void setCAVerifKeyMod(byte[] buffer, short offset) {
-		caVerificationKey.setModulus(buffer, offset, SIZE_RSAKEY_MOD);
+		caVerificationKey.setModulus(buffer, offset, SIZE_RSA_KEY_MOD);
 	}
 
-	/** Set the data part of the certificate */
+	/** Set the data part of the smartcard certificate */
 	void setCertData(byte[] buffer, short ofs) {
-		Util.arrayCopy(buffer, ofs, certificate, (short) 0,
-				SIZE_CERTIFICATE_DATA);
+		Util
+				.arrayCopy(buffer, ofs, certificate, (short) 0,
+						SIZE_CERT_DATA_CARD);
 	}
 
 	/** Set the CA signature part of the certificate */
 	void setCertSig(byte[] buffer, short ofs) {
-		Util.arrayCopy(buffer, ofs, certificate, SIZE_CERTIFICATE_DATA,
-				SIZE_CERTIFICATE_SIG);
-	}
-
-	/** Get the certificate in the buffer data */
-	short getCert(byte[] buf, short ofs) {
-		short len = SIZE_CERTIFICATE_DATA + SIZE_CERTIFICATE_SIG;
-		Util.arrayCopy(certificate, (short) 0, buf, ofs, len);
-		return len;
+		Util.arrayCopy(buffer, ofs, certificate, SIZE_CERT_DATA_CARD,
+				SIZE_RSA_SIG);
 	}
 
 }
--- Smartcard/src/smartcar/Smartcard.java
+++ Smartcard/src/smartcar/Smartcard.java
@@ -5,9 +5,13 @@ import javacard.framework.Applet;
 import javacard.framework.ISO7816;
 import javacard.framework.ISOException;
 import javacard.framework.JCSystem;
-import javacard.framework.TransactionException;
+import javacard.framework.Util;
 
 public class Smartcard extends Applet {
+
+	// Default format of APDUs
+	public static final byte CLA_CRYPTO = (byte) 0xCA;
+	public static final short P1P2 = (short) 0x0000;
 
 	// INStruction codes
 	public static final byte INS_PT_PERSONALIZE_SK = 0x10;
@@ -28,32 +32,53 @@ public class Smartcard extends Applet {
 	public static final byte INS_RT_TOPUP_KM_7 = 0x54;
 	public static final byte INS_RT_REFUND_KM_7 = 0x56;
 
+	// Expected message lengths
+	public static final short SIZE_PT_PERSONALIZE_SK = 0x10;
+	public static final short SIZE_PT_PERSONALIZE_VKCA = 0x12;
+	public static final short SIZE_PT_PERSONALIZE_CERT_DATA = 0x14;
+	public static final short SIZE_PT_PERSONALIZE_CERT_SIG = 0x16;
+	public static final short SIZE_AUTH_1 = 0x20;
+	public static final short SIZE_AUTH_2 = 0x22;
+	public static final short SIZE_AUTH_3 = 0x24;
+	public static final short SIZE_AUTH_4 = 0x26;
+	public static final short SIZE_AUTH_5 = 0x28;
+	public static final short SIZE_AUTH_6 = 0x2A;
+	public static final short SIZE_VT_START = 0x40;
+	public static final short SIZE_VT_TICK_KM = 0x42;
+	public static final short SIZE_VT_STOP = 0x44;
+	public static final short SIZE_RT_RENEW_CERT = 0x50;
+	public static final short SIZE_RT_TOPUP_KM_3 = 0x52;
+	public static final short SIZE_RT_TOPUP_KM_7 = 0x54;
+	public static final short SIZE_RT_REFUND_KM_7 = 0x56;
+
 	// State for receiving multi-APDU data
 	public static final byte STATE_INIT = 0x00;
 	public static final byte STATE_PERSONALIZED = 0x01;
 	public static final byte STATE_TERMINATED = 0x02;
 
-	// Terminal type identifier
-	public static final byte TYPE_TERMINAL_PT = 0x01;
-	public static final byte TYPE_TERMINAL_RT = 0x02;
-	public static final byte TYPE_TERMINAL_VT = 0x03;
+	// Terminal type identifier (corresponds with certificate types)
+	public static final byte TYPE_TERMINAL_RT = 0x01;
+	public static final byte TYPE_TERMINAL_VT = 0x02;
+	// public static final byte TYPE_TERMINAL_PT = 0x03;
 
 	// Permanent card state
 	private byte state;
 
-	/**
-	 *  Transient card state:
-	 *    protocolState[0]: previous instruction
-	 *    protocolState[1]: terminal type
-	 *    protocolState[2]: is terminal authenticated?
+	/*
+	 * Transient card state: protocolState[0]: previous instruction;
+	 * protocolState[1]: terminal type; protocolState[2]: achieved multiple
+	 * authentication?
 	 */
 	private byte[] protocolState;
 
 	// Crypto material
-	private SecureData storage;
+	private SecureData sd;
 
 	// Private storage km
 	private short km;
+
+	// RAM buffer
+	byte[] tmp;
 
 	public static void install(byte[] bArray, short bOffset, byte bLength) {
 		// GP-compliant JavaCard applet registration
@@ -62,10 +87,12 @@ public class Smartcard extends Applet {
 	}
 
 	private Smartcard() {
-		storage = new SecureData();
+		sd = new SecureData();
 		state = STATE_INIT;
 		protocolState = JCSystem.makeTransientByteArray((short) 3,
 				JCSystem.CLEAR_ON_DESELECT);
+		tmp = JCSystem.makeTransientByteArray((short) 0x0300,
+				JCSystem.CLEAR_ON_DESELECT); // TODO: tighter length
 	}
 
 	public void process(APDU apdu) {
@@ -78,70 +105,80 @@ public class Smartcard extends Applet {
 		byte ins = buf[ISO7816.OFFSET_INS];
 		short lc = (short) (buf[ISO7816.OFFSET_LC] & 0x00FF);
 
-		// FIXME: check the lc when/before receiving, this could lead to
-		// bugs/vulnerabilities
+		try {
 
-		// FIXME: check the state when sending multiple data
+			// check the instruction format (might not be necessary)
+			if (buf[ISO7816.OFFSET_CLA] != CLA_CRYPTO) {
+				protocolState[0] = ins;
+				ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
+			}
+			if (Util.getShort(buf, ISO7816.OFFSET_P1) != P1P2) {
+				protocolState[0] = ins;
+				ISOException.throwIt(ISO7816.SW_INCORRECT_P1P2);
+			}
 
-		// Check the state of the smartcard
-		switch (state) {
-		case STATE_INIT:
-			switch (ins) {
-			case INS_PT_PERSONALIZE_SK:
-				personalizeSK(apdu, buf);
+			// FIXME: check the lc when/before receiving, not doing this could
+			// lead
+			// to bugs/vulnerabilities
+
+			// Check the state of the smartcard
+
+			switch (state) {
+			case STATE_INIT:
+				switch (ins) {
+				case INS_PT_PERSONALIZE_SK:
+					personalizeSK(apdu, buf);
+					break;
+				case INS_PT_PERSONALIZE_VKCA:
+					personalizeVKCA(apdu, buf);
+					break;
+				case INS_PT_PERSONALIZE_CERT_DATA:
+					personalizeCertData(apdu, buf);
+					break;
+				case INS_PT_PERSONALIZE_CERT_SIG:
+					personalizeCertSig(apdu, buf);
+					sd.init();
+					state = STATE_PERSONALIZED;
+					break;
+				default:
+					ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+				}
 				break;
-			case INS_PT_PERSONALIZE_VKCA:
-				personalizeVKCA(apdu, buf);
-				break;
-			case INS_PT_PERSONALIZE_CERT_DATA:
-				personalizeCertData(apdu, buf);
-				break;
-			case INS_PT_PERSONALIZE_CERT_SIG:
-				personalizeCertSig(apdu, buf);
-				// FIXME: check that this is the last step in personalization
-				storage.init();
-				state = STATE_PERSONALIZED;
+			case STATE_PERSONALIZED:
+				switch (ins) {
+				case INS_AUTH_1:
+					authenticate1(apdu, buf);
+					break;
+				case INS_AUTH_2:
+					authenticate2(apdu, buf);
+					break;
+				case INS_AUTH_3:
+					authenticate3(apdu, buf);
+					break;
+				case INS_AUTH_4:
+					authenticate4(apdu, buf);
+					break;
+				case INS_AUTH_5:
+					authenticate5(apdu, buf);
+					break;
+				case INS_AUTH_6:
+					authenticate6(apdu, buf);
+					break;
+				default:
+					ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+				}
 				break;
 			default:
-				protocolState[0] = ins;
-				ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+				ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
 			}
-			break;
-		case STATE_PERSONALIZED:
-			switch (ins) {
-			case INS_AUTH_1:
-				authenticate1(apdu, buf);
-				break;
-			case INS_AUTH_2:
-				authenticate2(apdu, buf);
-				break;
-			case INS_AUTH_3:
-				authenticate3(apdu, buf);
-				break;
-			case INS_AUTH_4:
-				authenticate4(apdu, buf);
-				break;
-			case INS_AUTH_5:
-				authenticate5(apdu, buf);
-				break;
-			case INS_AUTH_6:
-				authenticate6(apdu, buf);
-				break;
-			default:
-				protocolState[0] = ins;
-				ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
-			}
-			break;
-		default:
+		} finally {
 			protocolState[0] = ins;
-			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
 		}
-		protocolState[0] = ins;
 	}
 
 	private void personalizeSK(APDU apdu, byte[] buf) {
 		apdu.setIncomingAndReceive();
-		storage.setSignKeyExp(buf, ISO7816.OFFSET_CDATA);
+		sd.setSignKeyExp(buf, ISO7816.OFFSET_CDATA);
 	}
 
 	private void personalizeVKCA(APDU apdu, byte[] buf) {
@@ -149,7 +186,7 @@ public class Smartcard extends Applet {
 			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
 		}
 		apdu.setIncomingAndReceive();
-		storage.setCAVerifKeyMod(buf, ISO7816.OFFSET_CDATA);
+		sd.setCAVerifKeyMod(buf, ISO7816.OFFSET_CDATA);
 	}
 
 	private void personalizeCertData(APDU apdu, byte[] buf) {
@@ -157,8 +194,8 @@ public class Smartcard extends Applet {
 			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
 		}
 		apdu.setIncomingAndReceive();
-		storage.setCertData(buf, ISO7816.OFFSET_CDATA);
-		storage.setSignKeyMod(buf, (short) (ISO7816.OFFSET_CDATA + 1));
+		sd.setCertData(buf, ISO7816.OFFSET_CDATA);
+		sd.setSignKeyMod(buf, (short) (ISO7816.OFFSET_CDATA + 1));
 	}
 
 	private void personalizeCertSig(APDU apdu, byte[] buf) {
@@ -166,45 +203,78 @@ public class Smartcard extends Applet {
 			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
 		}
 		apdu.setIncomingAndReceive();
-		storage.setCertSig(buf, ISO7816.OFFSET_CDATA);
+		sd.setCertSig(buf, ISO7816.OFFSET_CDATA);
 	}
-	
+
+	/** Protocols 6.4 and 6.5 (mutual auth), step 1 (part 1) */
 	private void authenticate1(APDU apdu, byte[] buf) {
-		ISOException.throwIt(ISO7816.SW_FUNC_NOT_SUPPORTED);
+		apdu.setIncomingAndReceive();
+		// read the certificate data
+		Util.arrayCopy(buf, (short) 0, tmp, (short) 0,
+				SecureData.SIZE_CERT_DATA_TERM);
 	}
-	
+
+	/** Protocols 6.4 and 6.5 (mutual auth), step 1 (part 2) and step 2 (part 1) */
 	private void authenticate2(APDU apdu, byte[] buf) {
-		if (protocolState[0] != INS_AUTH_1) {
+		if (protocolState[0] != INS_AUTH_2) {
 			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
 		}
-		ISOException.throwIt(ISO7816.SW_FUNC_NOT_SUPPORTED);
+
+		apdu.setIncomingAndReceive();
+		// read the certificate signature
+		Util.arrayCopy(buf, (short) 0, tmp, SecureData.SIZE_CERT_DATA_TERM,
+				SecureData.SIZE_RSA_SIG);
+
+		// validate the terminal certificate
+		if (!sd.validateCert(tmp, (short) 0, tmp,
+				SecureData.SIZE_CERT_DATA_TERM)) {
+			ISOException.throwIt(ISO7816.SW_SECURITY_STATUS_NOT_SATISFIED);
+		}
+
+		// store certificate properties only after validation of the cert
+		protocolState[1] = tmp[0]; // set type of terminal
+		sd.setTermEncryptKeyMod(tmp, (short) 1);
+		// move nonce to correct position
+		Util.arrayCopy(tmp, SecureData.SIZE_RSA_KEY_MOD, tmp,
+				SecureData.SIZE_CERT_CARD, SecureData.SIZE_NONCE);
+
+		// create the unencrypted response
+		sd.createAuthResponse(tmp);
+
+		// send back the first encrypted blob
+		sd.publicEncrypt(tmp, (byte) 0, buf);
+		apdu.setOutgoingAndSend((short) 0, SecureData.SIZE_PUBENC_CIPH);
 	}
-	
+
 	private void authenticate3(APDU apdu, byte[] buf) {
 		if (protocolState[0] != INS_AUTH_2) {
 			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
 		}
-		ISOException.throwIt(ISO7816.SW_FUNC_NOT_SUPPORTED);
+		sd.publicEncrypt(tmp, (byte) 0, buf);
+		apdu.setOutgoingAndSend((short) 0, SecureData.SIZE_PUBENC_CIPH);
 	}
-	
+
 	private void authenticate4(APDU apdu, byte[] buf) {
 		if (protocolState[0] != INS_AUTH_3) {
 			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
 		}
-		ISOException.throwIt(ISO7816.SW_FUNC_NOT_SUPPORTED);
+		sd.publicEncrypt(tmp, (byte) 0, buf);
+		apdu.setOutgoingAndSend((short) 0, SecureData.SIZE_PUBENC_CIPH);
 	}
-	
+
 	private void authenticate5(APDU apdu, byte[] buf) {
 		if (protocolState[0] != INS_AUTH_4) {
 			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
 		}
-		ISOException.throwIt(ISO7816.SW_FUNC_NOT_SUPPORTED);
+		sd.publicEncrypt(tmp, (byte) 0, buf);
+		apdu.setOutgoingAndSend((short) 0, SecureData.SIZE_PUBENC_CIPH);
 	}
-	
+
 	private void authenticate6(APDU apdu, byte[] buf) {
 		if (protocolState[0] != INS_AUTH_5) {
 			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
 		}
-		ISOException.throwIt(ISO7816.SW_FUNC_NOT_SUPPORTED);
+		sd.publicEncrypt(tmp, (byte) 0, buf);
+		apdu.setOutgoingAndSend((short) 0, SecureData.SIZE_PUBENC_CIPH);
 	}
 }
