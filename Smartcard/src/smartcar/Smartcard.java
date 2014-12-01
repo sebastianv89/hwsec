1b79ac56ca556fdc01ad877554cef43affa63952
Sebastian <sebastian.verschoor@gmail.com>
2014-12-01 01:30:57 +0100

    Implemented most of the smartcard

--- Smartcard/src/smartcar/Smartcard.java
+++ Smartcard/src/smartcar/Smartcard.java
@@ -7,7 +7,7 @@ import javacard.framework.ISOException;
 import javacard.framework.JCSystem;
 import javacard.framework.Util;
 
-public class Smartcard extends Applet {
+public class Smartcard extends Applet implements ISO7816 {
 
 	// Default format of APDUs
 	public static final byte CLA_CRYPTO = (byte) 0xCA;
@@ -24,32 +24,34 @@ public class Smartcard extends Applet {
 	public static final byte INS_AUTH_4 = 0x26;
 	public static final byte INS_AUTH_5 = 0x28;
 	public static final byte INS_AUTH_6 = 0x2A;
+	public static final byte INS_AUTH_7 = 0x2C;
 	public static final byte INS_VT_START = 0x40;
 	public static final byte INS_VT_TICK_KM = 0x42;
 	public static final byte INS_VT_STOP = 0x44;
-	public static final byte INS_RT_RENEW_CERT = 0x50;
-	public static final byte INS_RT_TOPUP_KM_3 = 0x52;
-	public static final byte INS_RT_TOPUP_KM_7 = 0x54;
-	public static final byte INS_RT_REFUND_KM_7 = 0x56;
+	public static final byte INS_RT_RENEW_CERT_1 = 0x50;
+	public static final byte INS_RT_RENEW_CERT_2 = 0x52;
+	public static final byte INS_RT_TOPUP_KM = 0x54;
+	public static final byte INS_RT_REFUND_KM = 0x56;
 
-	// Expected message lengths
-	public static final short SIZE_PT_PERSONALIZE_SK = 0x10;
-	public static final short SIZE_PT_PERSONALIZE_VKCA = 0x12;
-	public static final short SIZE_PT_PERSONALIZE_CERT_DATA = 0x14;
-	public static final short SIZE_PT_PERSONALIZE_CERT_SIG = 0x16;
-	public static final short SIZE_AUTH_1 = 0x20;
-	public static final short SIZE_AUTH_2 = 0x22;
-	public static final short SIZE_AUTH_3 = 0x24;
-	public static final short SIZE_AUTH_4 = 0x26;
-	public static final short SIZE_AUTH_5 = 0x28;
-	public static final short SIZE_AUTH_6 = 0x2A;
-	public static final short SIZE_VT_START = 0x40;
-	public static final short SIZE_VT_TICK_KM = 0x42;
-	public static final short SIZE_VT_STOP = 0x44;
-	public static final short SIZE_RT_RENEW_CERT = 0x50;
-	public static final short SIZE_RT_TOPUP_KM_3 = 0x52;
-	public static final short SIZE_RT_TOPUP_KM_7 = 0x54;
-	public static final short SIZE_RT_REFUND_KM_7 = 0x56;
+	// TODO: Expected message lengths
+	public static final short SIZE_PT_PERSONALIZE_SK = 0x00;
+	public static final short SIZE_PT_PERSONALIZE_VKCA = 0x00;
+	public static final short SIZE_PT_PERSONALIZE_CERT_DATA = 0x00;
+	public static final short SIZE_PT_PERSONALIZE_CERT_SIG = 0x00;
+	public static final short SIZE_AUTH_1 = 0x00;
+	public static final short SIZE_AUTH_2 = 0x00;
+	public static final short SIZE_AUTH_3 = 0x00;
+	public static final short SIZE_AUTH_4 = 0x00;
+	public static final short SIZE_AUTH_5 = 0x00;
+	public static final short SIZE_AUTH_6 = 0x00;
+	public static final short SIZE_AUTH_7 = 0x00;
+	public static final short SIZE_VT_START = 0x00;
+	public static final short SIZE_VT_TICK_KM = 0x00;
+	public static final short SIZE_VT_STOP = 0x00;
+	public static final short SIZE_RT_RENEW_CERT = 0x00;
+	public static final short SIZE_RT_TOPUP_KM_3 = 0x00;
+	public static final short SIZE_RT_TOPUP_KM_7 = 0x00;
+	public static final short SIZE_RT_REFUND_KM_7 = 0x00;
 
 	// State for receiving multi-APDU data
 	public static final byte STATE_INIT = 0x00;
@@ -59,7 +61,22 @@ public class Smartcard extends Applet {
 	// Terminal type identifier (corresponds with certificate types)
 	public static final byte TYPE_TERMINAL_RT = 0x01;
 	public static final byte TYPE_TERMINAL_VT = 0x02;
-	// public static final byte TYPE_TERMINAL_PT = 0x03;
+	public static final byte TYPE_TERMINAL_PT = 0x03;
+
+	// Protocol states
+	public static final byte PROT_STATE_VEHICLE_STARTED = 0x01;
+
+	// Messages
+	public static final byte MSG_IGNITION = 0x45;
+	public static final byte MSG_IGNITION_OK = 0x46;
+	public static final byte MSG_DEDUCT_KM = 0x48;
+	public static final byte MSG_DEDUCT_OK = 0x49;
+	public static final byte MSG_NOT_ENOUGH_KM = 0x47;
+	public static final byte MSG_STOP = 0x48;
+	public static final byte MSG_STOP_OK = 0x49;
+	public static final byte MSG_TOPUP = 0x4a;
+	public static final byte MSG_TOPUP_OK = 0x4b;
+	public static final byte MSG_REFUND_OK = 0x4c;
 
 	// Permanent card state
 	private byte state;
@@ -67,7 +84,7 @@ public class Smartcard extends Applet {
 	/*
 	 * Transient card state: protocolState[0]: previous instruction;
 	 * protocolState[1]: terminal type; protocolState[2]: achieved multiple
-	 * authentication?
+	 * authentication?; protocolState[3]: ignited
 	 */
 	private byte[] protocolState;
 
@@ -75,7 +92,7 @@ public class Smartcard extends Applet {
 	private SecureData sd;
 
 	// Private storage km
-	private short km;
+	private Kilometer km;
 
 	// RAM buffer
 	byte[] tmp;
@@ -89,7 +106,7 @@ public class Smartcard extends Applet {
 	private Smartcard() {
 		sd = new SecureData();
 		state = STATE_INIT;
-		protocolState = JCSystem.makeTransientByteArray((short) 3,
+		protocolState = JCSystem.makeTransientByteArray((short) 4,
 				JCSystem.CLEAR_ON_DESELECT);
 		tmp = JCSystem.makeTransientByteArray((short) 0x0300,
 				JCSystem.CLEAR_ON_DESELECT); // TODO: tighter length
@@ -102,19 +119,19 @@ public class Smartcard extends Applet {
 		}
 
 		byte[] buf = apdu.getBuffer();
-		byte ins = buf[ISO7816.OFFSET_INS];
-		short lc = (short) (buf[ISO7816.OFFSET_LC] & 0x00FF);
+		byte ins = buf[OFFSET_INS];
+		short lc = (short) (buf[OFFSET_LC] & 0x00FF);
 
 		try {
 
 			// check the instruction format (might not be necessary)
-			if (buf[ISO7816.OFFSET_CLA] != CLA_CRYPTO) {
-				protocolState[0] = ins;
-				ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
+			if (buf[OFFSET_CLA] != CLA_CRYPTO) {
+				setLastIns(ins);
+				ISOException.throwIt(SW_CLA_NOT_SUPPORTED);
 			}
-			if (Util.getShort(buf, ISO7816.OFFSET_P1) != P1P2) {
-				protocolState[0] = ins;
-				ISOException.throwIt(ISO7816.SW_INCORRECT_P1P2);
+			if (Util.getShort(buf, OFFSET_P1) != P1P2) {
+				setLastIns(ins);
+				ISOException.throwIt(SW_INCORRECT_P1P2);
 			}
 
 			// FIXME: check the lc when/before receiving, not doing this could
@@ -130,18 +147,30 @@ public class Smartcard extends Applet {
 					personalizeSK(apdu, buf);
 					break;
 				case INS_PT_PERSONALIZE_VKCA:
-					personalizeVKCA(apdu, buf);
+					if (getLastIns() == INS_PT_PERSONALIZE_SK) {
+						personalizeVKCA(apdu, buf);
+					} else {
+						ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
+					}
 					break;
 				case INS_PT_PERSONALIZE_CERT_DATA:
-					personalizeCertData(apdu, buf);
+					if (getLastIns() == INS_PT_PERSONALIZE_VKCA) {
+						personalizeCertData(apdu, buf);
+					} else {
+						ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
+					}
 					break;
 				case INS_PT_PERSONALIZE_CERT_SIG:
-					personalizeCertSig(apdu, buf);
-					sd.init();
-					state = STATE_PERSONALIZED;
+					if (getLastIns() == INS_PT_PERSONALIZE_CERT_DATA) {
+						personalizeCertSig(apdu, buf);
+						sd.init();
+						state = STATE_PERSONALIZED;
+					} else {
+						ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
+					}
 					break;
 				default:
-					ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+					ISOException.throwIt(SW_INS_NOT_SUPPORTED);
 				}
 				break;
 			case STATE_PERSONALIZED:
@@ -150,60 +179,133 @@ public class Smartcard extends Applet {
 					authenticate1(apdu, buf);
 					break;
 				case INS_AUTH_2:
-					authenticate2(apdu, buf);
+					if (getLastIns() == INS_AUTH_1) {
+						authenticate2(apdu, buf);
+					} else {
+						ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
+					}
 					break;
 				case INS_AUTH_3:
-					authenticate3(apdu, buf);
+					if (getLastIns() == INS_AUTH_2) {
+						authenticate3(apdu, buf);
+					} else {
+						ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
+					}
 					break;
 				case INS_AUTH_4:
-					authenticate4(apdu, buf);
+					if (getLastIns() == INS_AUTH_3) {
+						authenticate4(apdu, buf);
+					} else {
+						ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
+					}
 					break;
 				case INS_AUTH_5:
-					authenticate5(apdu, buf);
+					if (getLastIns() == INS_AUTH_4) {
+						authenticate5(apdu, buf);
+					} else {
+						ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
+					}
 					break;
 				case INS_AUTH_6:
-					authenticate6(apdu, buf);
+					if (getLastIns() == INS_AUTH_5) {
+						authenticate6(apdu, buf);
+					} else {
+						ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
+					}
+					break;
+				case INS_AUTH_7:
+					if (getLastIns() == INS_AUTH_6) {
+						authenticate7(apdu, buf);
+					} else {
+						ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
+					}
+					break;
+				case INS_VT_START:
+					if (isAuthenticated() && getTermType() == TYPE_TERMINAL_VT) {
+						startVehicle(apdu, buf);
+					} else {
+						ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
+					}
+					break;
+				case INS_VT_TICK_KM:
+					if (isAuthenticated() && getTermType() == TYPE_TERMINAL_VT
+							&& isIgnited()) {
+						tick(apdu, buf);
+					} else {
+						ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
+					}
+					break;
+				case INS_VT_STOP:
+					if (isAuthenticated() && getTermType() == TYPE_TERMINAL_VT
+							&& isIgnited()) {
+						stopVehicle(apdu, buf);
+					} else {
+						ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
+					}
+					break;
+				case INS_RT_RENEW_CERT_1:
+					if (isAuthenticated() && getTermType() == TYPE_TERMINAL_RT
+							&& getLastIns() == INS_AUTH_7) {
+						renewCertificate(apdu, buf);
+					} else {
+						ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
+					}
+					break;
+				case INS_RT_RENEW_CERT_2:
+					if (isAuthenticated() && getTermType() == TYPE_TERMINAL_RT
+							&& getLastIns() == INS_RT_RENEW_CERT_1) {
+						topup(apdu, buf);
+					} else {
+						ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
+					}
+					break;
+				case INS_RT_TOPUP_KM:
+					if (isAuthenticated() && getTermType() == TYPE_TERMINAL_RT
+							&& getLastIns() == INS_RT_RENEW_CERT_2) {
+						topup(apdu, buf);
+					} else {
+						ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
+					}
+					break;
+				case INS_RT_REFUND_KM:
+					if (isAuthenticated() && getTermType() == TYPE_TERMINAL_RT
+							&& getLastIns() == INS_RT_RENEW_CERT_2) {
+						refund(apdu, buf);
+					} else {
+						ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
+					}
 					break;
 				default:
-					ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+					ISOException.throwIt(SW_INS_NOT_SUPPORTED);
 				}
 				break;
 			default:
-				ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
+				ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
 			}
 		} finally {
-			protocolState[0] = ins;
+			setLastIns(ins);
 		}
 	}
 
 	private void personalizeSK(APDU apdu, byte[] buf) {
 		apdu.setIncomingAndReceive();
-		sd.setSignKeyExp(buf, ISO7816.OFFSET_CDATA);
+		sd.setSignKeyExp(buf, OFFSET_CDATA);
 	}
 
 	private void personalizeVKCA(APDU apdu, byte[] buf) {
-		if (protocolState[0] != INS_PT_PERSONALIZE_SK) {
-			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
-		}
 		apdu.setIncomingAndReceive();
-		sd.setCAVerifKeyMod(buf, ISO7816.OFFSET_CDATA);
+		sd.setCAVerifKeyMod(buf, OFFSET_CDATA);
 	}
 
 	private void personalizeCertData(APDU apdu, byte[] buf) {
-		if (protocolState[0] != INS_PT_PERSONALIZE_VKCA) {
-			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
-		}
 		apdu.setIncomingAndReceive();
-		sd.setCertData(buf, ISO7816.OFFSET_CDATA);
-		sd.setSignKeyMod(buf, (short) (ISO7816.OFFSET_CDATA + 1));
+		sd.setCertData(buf, OFFSET_CDATA);
+		sd.setSignKeyMod(buf, (short) (OFFSET_CDATA + 1));
 	}
 
 	private void personalizeCertSig(APDU apdu, byte[] buf) {
-		if (protocolState[0] != INS_PT_PERSONALIZE_CERT_DATA) {
-			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
-		}
 		apdu.setIncomingAndReceive();
-		sd.setCertSig(buf, ISO7816.OFFSET_CDATA);
+		sd.setCertSig(buf, OFFSET_CDATA);
 	}
 
 	/** Protocols 6.4 and 6.5 (mutual auth), step 1 (part 1) */
@@ -216,10 +318,6 @@ public class Smartcard extends Applet {
 
 	/** Protocols 6.4 and 6.5 (mutual auth), step 1 (part 2) and step 2 (part 1) */
 	private void authenticate2(APDU apdu, byte[] buf) {
-		if (protocolState[0] != INS_AUTH_2) {
-			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
-		}
-
 		apdu.setIncomingAndReceive();
 		// read the certificate signature
 		Util.arrayCopy(buf, (short) 0, tmp, SecureData.SIZE_CERT_DATA_TERM,
@@ -228,17 +326,17 @@ public class Smartcard extends Applet {
 		// validate the terminal certificate
 		if (!sd.validateCert(tmp, (short) 0, tmp,
 				SecureData.SIZE_CERT_DATA_TERM)) {
-			ISOException.throwIt(ISO7816.SW_SECURITY_STATUS_NOT_SATISFIED);
+			ISOException.throwIt(SW_SECURITY_STATUS_NOT_SATISFIED);
 		}
 
 		// store certificate properties only after validation of the cert
-		protocolState[1] = tmp[0]; // set type of terminal
-		sd.setTermEncryptKeyMod(tmp, (short) 1);
+		setTermType(tmp[0]);
+		sd.setPubEncryptKeyMod(tmp, (short) 1);
 		// move nonce to correct position
 		Util.arrayCopy(tmp, SecureData.SIZE_RSA_KEY_MOD, tmp,
 				SecureData.SIZE_CERT_CARD, SecureData.SIZE_NONCE);
 
-		// create the unencrypted response
+		// create the unencrypted response and set the temporary symmetric key
 		sd.createAuthResponse(tmp);
 
 		// send back the first encrypted blob
@@ -246,35 +344,187 @@ public class Smartcard extends Applet {
 		apdu.setOutgoingAndSend((short) 0, SecureData.SIZE_PUBENC_CIPH);
 	}
 
+	/** Protocols 6.4 and 6.5 (mutual auth), step 2 (part 2) */
 	private void authenticate3(APDU apdu, byte[] buf) {
-		if (protocolState[0] != INS_AUTH_2) {
-			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
-		}
-		sd.publicEncrypt(tmp, (byte) 0, buf);
+		sd.publicEncrypt(tmp, (byte) 1, buf);
 		apdu.setOutgoingAndSend((short) 0, SecureData.SIZE_PUBENC_CIPH);
 	}
 
+	/** Protocols 6.4 and 6.5 (mutual auth), step 2 (part 3) */
 	private void authenticate4(APDU apdu, byte[] buf) {
-		if (protocolState[0] != INS_AUTH_3) {
-			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
-		}
-		sd.publicEncrypt(tmp, (byte) 0, buf);
+		sd.publicEncrypt(tmp, (byte) 2, buf);
 		apdu.setOutgoingAndSend((short) 0, SecureData.SIZE_PUBENC_CIPH);
 	}
 
+	/** Protocols 6.4 and 6.5 (mutual auth), step 2 (part 4) */
 	private void authenticate5(APDU apdu, byte[] buf) {
-		if (protocolState[0] != INS_AUTH_4) {
-			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
-		}
-		sd.publicEncrypt(tmp, (byte) 0, buf);
+		sd.publicEncrypt(tmp, (byte) 3, buf);
 		apdu.setOutgoingAndSend((short) 0, SecureData.SIZE_PUBENC_CIPH);
 	}
 
+	/** Protocols 6.4 and 6.5 (mutual auth), step 2 (part 5) */
 	private void authenticate6(APDU apdu, byte[] buf) {
-		if (protocolState[0] != INS_AUTH_5) {
-			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
-		}
-		sd.publicEncrypt(tmp, (byte) 0, buf);
+		sd.publicEncrypt(tmp, (byte) 4, buf);
 		apdu.setOutgoingAndSend((short) 0, SecureData.SIZE_PUBENC_CIPH);
+	}
+
+	/** Protocols 6.4 and 6.5 (mutual auth), step 4 */
+	private void authenticate7(APDU apdu, byte[] buf) {
+		apdu.setIncomingAndReceive();
+		sd.sessionDecrypt(buf, OFFSET_CDATA, SecureData.SIZE_AES_BLOCKSIZE,
+				tmp, (short) 0);
+		sd.setSecretKey(tmp, (short) 0);
+		setAuthenticated(true); // TODO: is this reset to false?
+	}
+
+	/** Protocol 6.6 (vehicle ignition) */
+	private void startVehicle(APDU apdu, byte[] buf) {
+		// read ignition message (required for authentication)
+		apdu.setIncomingAndReceive();
+		sd.sessionDecrypt(buf, (short) 0, SecureData.SIZE_AES_BLOCKSIZE, tmp,
+				(short) 0);
+		if (tmp[0] != MSG_IGNITION) {
+			ISOException.throwIt(SW_WRONG_DATA);
+		}
+
+		short len;
+
+		if (km.start()) {
+			setIgnited(true);
+
+			// reply "ignition ok"
+			tmp[0] = MSG_IGNITION_OK;
+			Util.setShort(tmp, (short) 1, km.getKm());
+			len = 3;
+			sd.sign(tmp, (short) 0, len, tmp, len);
+			len += SecureData.SIZE_RSA_SIG;
+			sd.sessionEncrypt(tmp, (short) 0, len, buf, (short) 0);
+			// FIXME: len
+			apdu.setOutgoingAndSend((short) 0, len);
+		} else {
+			// reply "not enough km"
+			tmp[0] = MSG_NOT_ENOUGH_KM;
+			len = 1;
+			sd.sign(tmp, (short) 0, len, tmp, len);
+			len += SecureData.SIZE_RSA_SIG;
+			sd.sessionEncrypt(tmp, (short) 0, len, buf, (short) 0);
+			// FIXME: len
+			apdu.setOutgoingAndSend((short) 0, len);
+		}
+	}
+
+	/** Protocol 6.7 (driving) */
+	private void tick(APDU apdu, byte[] buf) {
+		// read tick message (required for authentication)
+		apdu.setIncomingAndReceive();
+		sd.sessionDecrypt(buf, (short) 0, SecureData.SIZE_AES_BLOCKSIZE, tmp,
+				(short) 0);
+		if (tmp[0] != MSG_DEDUCT_KM) {
+			ISOException.throwIt(SW_WRONG_DATA);
+		}
+
+		short len;
+
+		if (km.tick()) {
+			// reply "deduct km ok"
+			tmp[0] = MSG_DEDUCT_OK;
+			Util.setShort(tmp, (short) 1, km.getKm());
+			len = 3;
+			sd.sign(tmp, (short) 0, len, tmp, len);
+			len += SecureData.SIZE_RSA_SIG;
+			sd.sessionEncrypt(tmp, (short) 0, len, buf, (short) 0);
+			// FIXME: len
+			apdu.setOutgoingAndSend((short) 0, len);
+		} else {
+			// reply "not enough km"
+			tmp[0] = MSG_NOT_ENOUGH_KM;
+			len = 1;
+			sd.sign(tmp, (short) 0, len, tmp, len);
+			len += SecureData.SIZE_RSA_SIG;
+			sd.sessionEncrypt(tmp, (short) 0, len, buf, (short) 0);
+			// FIXME: len
+			apdu.setOutgoingAndSend((short) 0, len);
+		}
+	}
+
+	/** Protocol 6.8 (stopping the vehicle) */
+	private void stopVehicle(APDU apdu, byte[] buf) {
+		// read ignition message (required for authentication)
+		apdu.setIncomingAndReceive();
+		sd.sessionDecrypt(buf, (short) 0, SecureData.SIZE_AES_BLOCKSIZE, tmp,
+				(short) 0);
+		if (tmp[0] != MSG_STOP) {
+			ISOException.throwIt(SW_WRONG_DATA);
+		}
+
+		// step 4
+		sd.destroySessionKey();
+		setAuthenticated(false);
+		setIgnited(false);
+
+		// step 5
+		km.stop();
+
+		// step 6: reply "stop ok"
+		short len = 1;
+		tmp[0] = MSG_STOP_OK;
+		sd.sign(tmp, (short) 0, len, tmp, len);
+		len += SecureData.SIZE_RSA_SIG;
+		apdu.setOutgoingAndSend((short) 0, len);
+	}
+
+	/**
+	 * Protocol 6.9 (renewing the certificate), step 4 and 5. Protocol 6.10/6.11
+	 * (topup/refund), step 3
+	 */
+	private void renewCertificate(APDU apdu, byte[] buf) {
+		// FIXME: in our protocol, this message is not encrypted with the
+		// session key
+		apdu.setIncomingAndReceive();
+		sd.sessionDecrypt(buf, (short) 0, SecureData.SIZE_AES_BLOCKSIZE, tmp,
+				(short) 0);
+		// TODO: implement
+	}
+
+	/** Protocol 6.10 (topup), step 7-9 */
+	private void topup(APDU apdu, byte[] buf) {
+		// TODO: implement
+	}
+
+	/** Protocol 6.11 (refund), step 8-9 */
+	private void refund(APDU apdu, byte[] buf) {
+		// TODO: implement
+	}
+
+	private void setLastIns(byte ins) {
+		protocolState[0] = ins;
+	}
+
+	private byte getLastIns() {
+		return protocolState[0];
+	}
+
+	private void setTermType(byte type) {
+		protocolState[1] = type;
+	}
+
+	private byte getTermType() {
+		return protocolState[1];
+	}
+
+	private void setAuthenticated(boolean auth) {
+		protocolState[2] = (byte) (auth ? 0x01 : 0x00);
+	}
+
+	private boolean isAuthenticated() {
+		return protocolState[2] == 0x01;
+	}
+
+	private void setIgnited(boolean ignited) {
+		protocolState[3] = (byte) (ignited ? 0x01 : 0x00);
+	}
+
+	private boolean isIgnited() {
+		return protocolState[3] == 0x01;
 	}
 }
