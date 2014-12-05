f4e0eeb41d567d10b0b30deff1bb2bad7e46cca0
Java Card Development <javacard@javacard-development.(none)>
2014-12-05 14:08:58 +0100

    gui euy

---
+++ Backend/src/terminal/Menu.java
@@ -0,0 +1,103 @@
+package terminal;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+
+public class Menu {
+
+	protected Shell shell;
+	protected static Menu window;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			window = new Menu();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+	
+	public void close(){
+		Display display = Display.getDefault();
+		while(!shell.isDisposed()){
+			display.dispose();
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		final RegisterNewCustomer regWindow = new RegisterNewCustomer();
+		
+		shell = new Shell();
+		shell.setSize(346, 300);
+		shell.setText("SWT Application");
+		
+		Label label = new Label(shell, SWT.NONE);
+		label.setText("Welcome to Cars Rental");
+		label.setAlignment(SWT.CENTER);
+		label.setBounds(10, 10, 324, 27);
+		
+		Label lblNewLabel = new Label(shell, SWT.NONE);
+		lblNewLabel.setBounds(28, 68, 390, 17);
+		lblNewLabel.setText("Insert your card and choose the menu below");
+		
+		final Button btnRadioButton_Register = new Button(shell, SWT.RADIO);
+		btnRadioButton_Register.setBounds(28, 108, 211, 22);
+		btnRadioButton_Register.setText("Register New Customer");
+		
+		final Button btnRadioButton_TopUp = new Button(shell, SWT.RADIO);
+		btnRadioButton_TopUp.setBounds(28, 136, 108, 22);
+		btnRadioButton_TopUp.setText("Top Up");
+		
+		final Button btnRadioButton_Refund = new Button(shell, SWT.RADIO);
+		btnRadioButton_Refund.setBounds(28, 164, 108, 22);
+		btnRadioButton_Refund.setText("Refund");
+		
+		Button btnSubmit = new Button(shell, SWT.NONE);
+		btnSubmit.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				String m = "";
+				if(btnRadioButton_Register.getSelection()){
+					m = "register";
+					window.close();
+					regWindow.open();
+				}else if(btnRadioButton_TopUp.getSelection()){
+					m = "top up";
+				}else if(btnRadioButton_Refund.getSelection()){
+					m = "refund";
+				}
+				//JOptionPane.showMessageDialog(null, m);
+			}
+		});
+		btnSubmit.setBounds(114, 204, 88, 29);
+		btnSubmit.setText("Submit");
+
+	}
+}
---
+++ Backend/src/terminal/RegisterNewCustomer.java
@@ -0,0 +1,87 @@
+package terminal;
+
+import javax.swing.JOptionPane;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+
+import backend.BackendRentalTerminal;
+import backend.ByteUtils;
+
+public class RegisterNewCustomer {
+
+	protected Shell shell;
+	private Text txt_CustName;
+	BackendRentalTerminal rt = new BackendRentalTerminal();
+	ByteUtils utils = new ByteUtils();
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			RegisterNewCustomer window = new RegisterNewCustomer();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(335, 281);
+		shell.setText("SWT Application");
+		
+		final Label lblNewCustomer = new Label(shell, SWT.NONE);
+		lblNewCustomer.setBounds(20, 199, 68, 17);
+		
+		Label lblCustomerName = new Label(shell, SWT.NONE);
+		lblCustomerName.setBounds(20, 65, 122, 17);
+		lblCustomerName.setText("Customer Name");
+		
+		Label lblCustomerRegistration = new Label(shell, SWT.NONE);
+		lblCustomerRegistration.setAlignment(SWT.CENTER);
+		lblCustomerRegistration.setText("Customer Registration");
+		lblCustomerRegistration.setBounds(55, 10, 196, 27);
+		
+		txt_CustName = new Text(shell, SWT.BORDER);
+		txt_CustName.setBounds(140, 65, 147, 27);
+		
+		Button btnSubmit = new Button(shell, SWT.NONE);
+		btnSubmit.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				rt.RegisterNewCustomer(txt_CustName.getText());
+				lblNewCustomer.setText(txt_CustName.getText());
+			}
+		});
+		btnSubmit.setBounds(121, 126, 88, 29);
+		btnSubmit.setText("Submit");
+		
+	}
+}
